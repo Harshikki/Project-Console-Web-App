c614e4a6a337d1beff2bf42c80ded29c885d8488
Harshikki <72389166+Harshikki@users.noreply.github.com>
2021-02-13 20:09:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("My First Console Project");
+
+var first_name = "Harshika";
+var last_name = "Vasili";
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var n1 = 5;
+var n2 = 25;
+
+var add = n1+n2;
+console.log(add);
+
+var sub = n2-n1;
+console.log(sub);
+
+var mul = n1*n2;
+console.log(mul);
+
+var div = n2/n1;
+console.log(div);
